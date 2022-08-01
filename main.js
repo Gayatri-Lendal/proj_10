cabccd0deed81ed716e2b82ce4c2e842a384df50
gayatrilendal323@gmail.com <gayatrilendal323@gmail.com>
2022-08-01 14:04:46 +0530

    save

---
+++ main.js
@@ -0,0 +1,87 @@
+console.log('Hello World!');
+
+// mod operator  
+
+var mod = 36 % 65;
+console.log(mod);
+
+var mod_1 = 88 % 4;
+console.log(mod_1);
+
+// Expotiation Operator
+
+var expo = 3 ** 92;
+console.log(expo);
+
+var expo_1 = 85 ** 8;
+console.log(expo_1);
+
+
+// Shorthand Operator
+
+var Jk = (7);
+Jk++;
+Jk--;
+console.log(Jk);
+
+var V = 3;
+V += 65;
+V -= 9;
+console.log(V);
+
+// Ternary Operator
+
+var numers_1 = 50;
+
+if (numers_1 >= 3.60) {
+  haha = " INDIA ";
+}
+else {
+  haha = "ARGENTINA";
+}
+
+console.log(haha);
+
+//.....
+
+numers = 50;
+
+var population = (numers <= 2.50) ? " Japan " : " Argentina";
+console.log(population);
+
+// TypeOf  Operator
+
+var string = "Jungkook";
+var type = typeof string;
+console.log(type);
+
+
+var int = 78;
+var type1 = typeof int;
+console.log(type1);
+
+
+var boolean = true;
+var type2 = typeof boolean;
+console.log(type2);
+
+// let or var scope  
+
+var gh = " priya ";
+var gh = "   Neera  ";
+console.log(gh);
+
+// oR
+
+let Y = "   Niranjana  ";
+console.log(Y);
+
+// Template literals 
+
+var monument = "Big Ben ";
+console.log(monument);
+
+var monument_1 = " Eiffel Tower ";
+
+var names = `MONUMENTS- ${monument} , ${monument_1}`;
+console.log(names);
